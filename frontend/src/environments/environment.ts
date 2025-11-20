196597ea6e272258e7af5b1c1ef6917a471ba56f
Umayal <umayal.murukappan@gmail.com>
2025-11-20 20:18:29 +0530

    Point Angular API base to /api on Render

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://farmbridge.onrender.com',
+  apiBaseUrl: 'https://farmbridge.onrender.com/api',
   googleClientId: '1059635223485-nq873adf9rk0nslp3ohco1357n153rp1.apps.googleusercontent.com'
 };
